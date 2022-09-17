90515ffee5769f87e7aab326219e9deb2589e761
Adwait Barkale <adwaitb@winjit.com>
2022-09-17 15:16:25 +0530

    Project Creation

---
+++ SwiftUI Bootcamp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Adwait Barkale on 17/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .font(.headline)
+            .fontWeight(.bold)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Bootcamp/SwiftUI_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BootcampApp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Adwait Barkale on 17/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
